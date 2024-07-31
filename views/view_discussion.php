8e3685fa7145977c76907640a6d65dad34dc06f2
angelica <uniunicornbae@gmail.com>
2024-08-01 06:06:43 +0800

    replies

--- views/view_discussion.php
+++ views/view_discussion.php
@@ -36,10 +36,12 @@
 
     <div class="discussion-view">
         <?php
+        session_start();
         include('../includes/db.php');
 
         // Get discussion_id from query string
         $discussion_id = isset($_GET['discussion_id']) ? intval($_GET['discussion_id']) : 0;
+        $user_id = isset($_SESSION['user_id']) ? intval($_SESSION['user_id']) : 0;
 
         // Fetch discussion details
         $discussionQuery = "SELECT * FROM discussion WHERE discussion_id = ?";
@@ -68,13 +70,14 @@
                     $commentsQuery = "
                         SELECT c.id, c.comment, c.date_created, u.username,
                             (SELECT COUNT(*) FROM comment_votes WHERE comment_id = c.id AND vote_type = 'upvote') AS upvote_count,
-                            (SELECT COUNT(*) FROM comment_votes WHERE comment_id = c.id AND vote_type = 'downvote') AS downvote_count
+                            (SELECT COUNT(*) FROM comment_votes WHERE comment_id = c.id AND vote_type = 'downvote') AS downvote_count,
+                            IFNULL((SELECT vote_type FROM comment_votes WHERE comment_id = c.id AND user_id = ? LIMIT 1), '') AS user_vote
                         FROM comments c
                         JOIN userdb u ON c.user_id = u.userID
                         WHERE c.discussion_id = ? ORDER BY c.date_created DESC
                     ";
                     $commentsStmt = $conn->prepare($commentsQuery);
-                    $commentsStmt->bind_param("i", $discussion_id);
+                    $commentsStmt->bind_param("ii", $user_id, $discussion_id);
                     $commentsStmt->execute();
                     $commentsResult = $commentsStmt->get_result();
 
@@ -85,13 +88,14 @@
                             $comment_text = htmlspecialchars($comment['comment']);
                             $upvote_count = $comment['upvote_count'];
                             $downvote_count = $comment['downvote_count'];
+                            $user_vote = $comment['user_vote'];
                     ?>
-                        <div class="comment">
+                        <div class="comment" id="comment-<?php echo $comment_id; ?>">
                             <p><strong><?php echo $username; ?>:</strong> <?php echo nl2br($comment_text); ?></p>
                             <div class="comment-actions">
                                 <button class="reply-button" data-comment-id="<?php echo $comment_id; ?>">Reply</button>
-                                <button class="upvote-button" data-comment-id="<?php echo $comment_id; ?>">Upvote (<?php echo $upvote_count; ?>)</button>
-                                <button class="downvote-button" data-comment-id="<?php echo $comment_id; ?>">Downvote (<?php echo $downvote_count; ?>)</button>
+                                <button class="upvote-button" data-comment-id="<?php echo $comment_id; ?>" data-vote-type="upvote" <?php echo ($user_vote === 'upvote' ? 'disabled' : ''); ?>>Upvote (<?php echo $upvote_count; ?>)</button>
+                                <button class="downvote-button" data-comment-id="<?php echo $comment_id; ?>" data-vote-type="downvote" <?php echo ($user_vote === 'downvote' ? 'disabled' : ''); ?>>Downvote (<?php echo $downvote_count; ?>)</button>
                                 <button class="report-button" data-comment-id="<?php echo $comment_id; ?>">Report</button>
                             </div>
                             <div class="reply-form" id="reply-form-<?php echo $comment_id; ?>" style="display:none;">
